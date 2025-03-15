ac341d35ec50e2257e8b2a417884312c8a2624e4
MohammedFares <xmohammedfares@gmail.com>
2025-03-15 06:13:47 +0300

    added interviewreport

---
+++ Muqabalati.Service/DTOs/InterviewReportDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muqabalati.Service.DTOs
+{
+    public class InterviewReportDto
+    {
+
+        public int FailAnswers { get; set; }
+        public int CorrectAnswers { get; set; }
+        public bool IsPassed { get; set; }
+        public int GPA { get; set; }
+
+        public List<RecommendationDto>? Recommendations { get; set; } = new List<RecommendationDto>();
+
+    }
+}
